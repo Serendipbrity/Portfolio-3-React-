edcea88a112dfe09571c2f4efde50597a2e331be
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-08-04 23:08:18 -0400

    deploy

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 //   <React.StrictMode>
@@ -11,11 +12,11 @@ import reportWebVitals from './reportWebVitals';
 //   </React.StrictMode>
 // );
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
