e868e3845aa9a59b2acc11bb766f7e5626f1fa5f
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-08-05 01:04:14 -0400

    resume link

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -10,9 +10,9 @@ function Nav() {
     <header>
       <nav>
         <ul class='flex-row' id='name'>Brittani Court
-          <li class='navLink'>
+          {/* <li class='navLink'>
             <a href="#about">About Me</a>
-          </li>
+          </li> */}
           <li class='navLink'>
             <a href="#projectHeader">Portfolio </a>
           </li>
@@ -20,7 +20,7 @@ function Nav() {
             <a href="#contact">Contact </a>
           </li>
           <li class='navLink'>
-            <a href="#resume">Resume </a>
+            <a href="https://docs.google.com/document/d/1mnrcTgO3zlY4ljqUmMeDe4T4X4y_y9suxLQ-7sMkS80/edit?usp=sharing">Resume Link</a>
           </li>
         </ul>
       </nav>
